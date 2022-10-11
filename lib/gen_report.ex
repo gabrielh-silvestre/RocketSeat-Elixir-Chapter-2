b8eb651c8ea01746be11a3509bedf397bffbb8d2
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-10-11 19:02:16 -0300

    refactor(lib/gen_report): make module more readable

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,46 +1,56 @@
+# GenReport.build("gen_report.csv")
 defmodule GenReport do
   alias GenReport.Parser
 
-  def build() do
-    {:error, "Insira o nome de um arquivo"}
-  end
+  @report_fields ["all_hours", "hours_per_month", "hours_per_year"]
+  @report_build Enum.reduce(@report_fields, %{}, &Map.put(&2, &1, %{}))
+
+  def build, do: {:error, "Insira o nome de um arquivo"}
 
   def build(file_name) do
     Parser.parse_file(file_name)
-    |> Enum.reduce(build_report(), fn data, acc ->
-      acc
-      |> update_all_hours(data)
-      |> update_hours_per_month(data)
-      |> update_hours_per_year(data)
+    |> Enum.reduce(@report_build, &proccess_report/2)
+  end
+
+  defp proccess_report([name, work_time, _, month, year], report) do
+    report
+    |> update_all_hours(name, work_time)
+    |> update_hours_per_month(name, work_time, month)
+    |> update_hours_per_year(name, work_time, year)
+  end
+
+  defp update_all_hours(report, name, work_time) do
+    report
+    |> Map.update("all_hours", %{}, fn person ->
+      person |> Map.update(name, work_time, &(&1 + work_time))
     end)
   end
 
-  defp update_all_hours(acc, [name, work_time, _, _, _]) do
-    acc
-    |> Map.update!("all_hours", fn hours ->
-      Map.update(hours, name, work_time, &(&1 + work_time))
+  defp update_hours_per_month(report, name, work_time, month) do
+    report
+    |> Map.update!("hours_per_month", fn person ->
+      Map.update(
+        person,
+        name,
+        %{month => work_time},
+        fn hours_per_month ->
+          Map.update(hours_per_month, month, work_time, &(&1 + work_time))
+        end
+      )
     end)
   end
 
-  defp update_hours_per_month(acc, [name, work_time, _, month, _]) do
-    acc
-    |> Map.update!("hours_per_month", fn hours ->
-      Map.update(hours, name, %{month => work_time}, fn hours_per_month ->
-        Map.update(hours_per_month, month, work_time, &(&1 + work_time))
-      end)
-    end)
-  end
-
-  defp update_hours_per_year(acc, [name, work_time, _, _, year]) do
-    acc
+  defp update_hours_per_year(report, name, work_time, year) do
+    report
     |> Map.update!("hours_per_year", fn hours ->
-      Map.update(hours, name, %{year => work_time}, fn hours_per_year ->
-        Map.update(hours_per_year, year, work_time, &(&1 + work_time))
-      end)
+      Map.update(
+        hours,
+        name,
+        %{year => work_time},
+        fn hours_per_year ->
+          Map.update(hours_per_year, year, work_time, &(&1 + work_time))
+        end
+      )
     end)
-  end
-
-  defp build_report do
-    %{"all_hours" => %{}, "hours_per_month" => %{}, "hours_per_year" => %{}}
   end
 end
